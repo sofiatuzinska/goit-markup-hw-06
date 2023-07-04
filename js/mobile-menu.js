e5a4e68a59eb485d8251510a18da6e4984f13e6f
sofiatuzinska <129293949+sofiatuzinska@users.noreply.github.com>
2023-07-04 17:15:07 +0300

    summary

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,9 +1,14 @@
- openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
+(() => {
+   const refs = {
+     openMenuBtn: document.querySelector("[data-menu-open]"),
+     closeMenuBtn: document.querySelector("[data-menu-close]"),
+     menu: document.querySelector("[data-menu]"),
+   };
+ 
+   refs.openMenuBtn.addEventListener("click", toggleMenu);
+   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+ 
+   function toggleMenu() {
+     refs.menu.classList.toggle("is-hidden");
+   }
+ })();
